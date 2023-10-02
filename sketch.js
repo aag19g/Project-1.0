0d87a0874b454b7f97edb031c10cfc920018103d
Alisha Galarza <aag19g@fsu.edu>
2023-10-02 11:33:11 -0400

    Added a bg image to index file

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let butt1, butt2, butt3;
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+
+//   let butt1 = createButton("click me");
+//   button.mouseClicked(function() {clicked(1)});
+//   let butt2 = createButton("click too");
+//   button.mouseClicked(function() {clicked(2)});
+//   let butt3 = createButton("click 3");
+//   button.mouseClicked(function() {clicked(3)});
+// }
+
+// function clicked(num){
+// console.log("clicked" + num);
+// }
 }
 
-function draw() {
-  background(125);
-}
