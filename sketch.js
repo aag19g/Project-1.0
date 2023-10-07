874becc4c5586e527fb4fb1dd0614ec6eaf7b28c
Alisha Galarza <aag19g@fsu.edu>
2023-10-07 14:18:17 -0400

    making the buttons give random images from file

--- sketch.js
+++ sketch.js
@@ -1,24 +1,39 @@
 let button;
 let button2;
-let press;
+let button3;
+let button4;
+let ads = [];
+let numAds = 4;
 let img;
-
-function preload(){
-  img = loadImage("like.png");
-}
 
 function setup(){
   noCanvas();
 
-  button = createButton("");
+  button = createButton("click");
   button.position(246,664);
-  button.mouseClicked(buttonclicked);
+  button.mouseClicked(buttonClicked);
 
-  button2 = createButton("clear");
-  button2.mousePressed(clearAd);
+  button2 = createButton("click");
+  button2.position(300,664);
+  button2.mouseClicked(buttonClicked);
+
+  button3 = createButton("click");
+  button3.position(350,664);
+  button3.mouseClicked(buttonClicked);
+
+  button4 = createButton("click");
+  button4.position(745,664);
+  button4.mouseClicked(buttonClicked);
 }
 
-function mouseClicked(){
-  let img = createImg("assets/ads-02.png")
-  img.position(50, 1)
+function buttonClicked(){
+  let ad1 = createImg("assets/ads01.png");
+  let ad2 = createImg("assets/ads02.png");
+  let ad3 = createImg("assets/ads03.png");
+  let ad4 = createImg("assets/ads04.png");
+  ads = [ads1,ads2,ads3,ads4];
+  let randoImg = randome(ads)
+  // let img = createImg("assets/ads01.png")
+  // img.position(50, 1)
+  image(randoImg,50, 1)
 }
