fcaa341f98f5b4be7412337af67d7dd21ba41a00
Alisha <144165734+aag19g@users.noreply.github.com>
2023-10-10 21:43:20 -0400

    Interface Completed

--- sketch.js
+++ sketch.js
@@ -3,25 +3,39 @@ let button2;
 let button3;
 let button4;
 let img;
+let button5;
+// let imgPost;
+
+// function preload(){
+//   imgPost = loadImage('post5.png');
+// }
 
 function setup(){
   noCanvas();
 
-  button = createButton("click");
-  button.position(246,664);
+  imgPostElement = createImg('post5.png');
+  imgPostElement.position(251, 115);
+  imgPostElement.size(530,530)
+
+  button = createButton(".");
+  button.position(247,664);
   button.mouseClicked(buttonClicked);
 
-  button2 = createButton("click");
-  button2.position(300,664);
+  button2 = createButton(".");
+  button2.position(297,664);
   button2.mouseClicked(buttonClicked);
 
-  button3 = createButton("click");
-  button3.position(350,664);
+  button3 = createButton(".");
+  button3.position(370,664);
   button3.mouseClicked(buttonClicked);
 
-  button4 = createButton("click");
+  button4 = createButton(".");
   button4.position(745,664);
-  button4.mouseClicked(buttonClicked);
+  button4.mouseClicked(buttonClicked2);
+
+  button5 = createButton(">");
+  button5.position(745, 400)
+  button5.mouseClicked(buttonPressed)
 }
 
 function buttonClicked(){
@@ -40,6 +54,17 @@ function buttonClicked(){
   img.position(50, 1)
 
   img.mousePressed(clearImage);
+
+
+}
+
+function buttonClicked2(){
+  console.log("cliked2");
+
+  img = createImg('ads05.png')
+  img.position(50, 1)
+
+  img.mousePressed(clearImage);
 }
 
 function clearImage(){
@@ -48,3 +73,18 @@ function clearImage(){
   }
 }
 
+function buttonPressed(){
+
+  let posts = [
+    "posts/post1.png",
+    "posts/post2.png",
+    "posts/post3.png",
+    "posts/post4.png",
+  ];
+
+  let randomIndex = floor(random(posts.length));
+  let randomImg = posts[randomIndex];
+  img = createImg(randomImg)
+  img.position(251,115)
+  img.size(530,530)
+}
---
+++ sketch2.js
@@ -0,0 +1,11 @@
+// let imgPostElement;
+
+// function setup(){
+// noCanvas();
+// imgPostElement = createImg('ads06.png');
+// imgPostElement.size(400,400)
+// }
+
+// function mouseMoved(){
+// imgPostElement.position(mouseX, mouseY);
+// }
